478c0d244a3e00402dbb0afe36b3c10a51d98fea
Jignesh Solanki <Solankijignesh4611@gmail.com>
2025-06-16 17:26:39 +0530

    impressive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RecipeChart from '../../Components/RecipeChart'
 
+
 const Home = () => {
+    const [searchQuery, setsearchQuery] = useState("")
     const recipes = [
         { id: 1, title: "Vadapaav", },
         { id: 2, title: "DalBati", },
         { id: 3, title: "Dhokla", },
         { id: 4, title: "samosa", },
     ]
-    const handleSearch = () => {
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
 
 
     }
@@ -17,12 +22,16 @@ const Home = () => {
             <form onSubmit={handleSearch} className='search-form'>
                 <input type='text'
                     placeholder='search recipe....'
-                    className='search-input' />
-                    <button type='submit' className="search-button">Search</button>
+                    className='search-input'
+                    value={searchQuery}
+                    onChange={(e) => setsearchQuery(e.target.value)}
+                />
+
+                <button type='submit' className="search-button">Search</button>
             </form>
             <div className="recipe-grid">
                 {recipes.map((recipe) => (
-                    <RecipeChart recipe={recipe} key={recipe.id} />
+                recipe.title.toLowerCase().startsWith(searchQuery) &&   <RecipeChart recipe={recipe} key={recipe.id} />
                 ))}
             </div>
         </div>
